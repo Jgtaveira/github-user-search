52d6e4be869cc1017a2098002c2cdd2217f3c421
Joao Taveira <jgsart22@gmail.com>
2021-04-14 01:04:14 -0300

    Navbar created

---
+++ github-user-search/src/core/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => (
+        <nav className="row nav-container" >
+            <div className="col-2 nav-logo-text" >
+                <h5>
+                    Bootcamp DevSuperior
+                </h5>
+            </div>
+        </nav>
+    )
+
+
+    export default Navbar;
